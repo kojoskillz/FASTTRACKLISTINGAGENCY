a0fe659a91f9ba0982f8481ea12bb8e890a89cde
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-08-05 19:54:36 +0000

    Update navbar.jsx

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import logo from './logo.png'
 
 const navbar = () => {
   return (
     <div>
       
         <nav className='glass_containerX pb-5 flex jockey-one-regular'>
-        <ul className='flex gap-5 text-white m-auto flex place-content-center font-semibold pt-4 '>
+        <img src={logo} alt="" className='logo absolute -mt-[3rem] w-44 h-44'  />
+        <ul className='flex gap-5 text-white m-auto flex place-content-center ul_list font-semibold pt-4 '>
+            
              <li><a href="#Home" className='cursor-pointer hover:text-red-600'>Home</a></li>
              <li><a href="#About Us" className='cursor-pointer hover:text-red-600'>About Us</a></li>
              <li><a href="#Services" className='cursor-pointer hover:text-red-600'>Services</a></li>
              <li><a href="#Contact Us" className='cursor-pointer hover:text-red-600'>Contact Us</a></li>
           </ul>
 
-          {/* <div  className='p-1 mt-2 mr-12 jockey-one-regular'>
-               <a className="btn" href="#">Join Now</a>
-          </div> */}
+       
         </nav>
+
+        {/* responsive navbar */}
+
 
     </div>
   )
