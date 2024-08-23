9a459a0b868ad739f5c81f53c3a2d54702c1979c
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-08-23 00:43:58 +0000

    Add files via upload

---
+++ src/utils/cn.tsx
@@ -0,0 +1,8 @@
+// ./src/utils/cn.tsx
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+              
